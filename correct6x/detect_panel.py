df017d6f90ade37d9629066dcc0fa944553dc37c
unknown <cliff.dellamano@sentera.com>
2020-09-09 17:02:10 -0500

    Fix panel dimension bug

--- correct6x/detect_panel.py
+++ correct6x/detect_panel.py
@@ -2,10 +2,8 @@ import logging
 
 import cv2 as cv
 import numpy as np
-import pandas as pd
-import glob
-from imgparse import imgparse
 
+from typing import NamedTuple, Tuple
 from PIL import Image
 
 # Constants
@@ -20,51 +18,53 @@ SAMPLE_RECT_WIDTH = 0.04
 logger = logging.getLogger(__name__)
 
 
+class BoundingBox(NamedTuple):
+    """
+    Lightweight class representing a non-rotated bounding box as a pair of top left and bottom right (X, Y) points.
+    """
+    top_left: Tuple[int, int]
+    bottom_right: Tuple[int, int]
+
+    def bounds(self):
+        """Return a sequence of slice objects that allow for use of the bounding box in NumPy indexing expressions."""
+        return slice(self.top_left[1], self.bottom_right[1]), slice(self.top_left[0], self.bottom_right[0])
+
+
 def convert_to_type(image, desired_type=np.uint8):
     """
     Converts the 12-bit tiff from a 6X sensor to a numpy compatible form
     :param desired_type: The desired type
     :return: The converted image in numpy.array format
     """
-    image = image / MAX_VAL_12BIT # Scale to 0-1
-    image = np.iinfo(desired_type).max * image # Scale back to desired type
+    image = image / MAX_VAL_12BIT  # Scale to 0-1
+    image = np.iinfo(desired_type).max * image  # Scale back to desired type
     return image.astype(desired_type)
-
-
-def generate_aruco_marker(id=13):
-    """
-    Generates and writes to disk an aruco label
-    :param id: The aruco id of a 6x6 block, 250x250 pixel dictionary, defaults 13
-    :return: True/False
-    """
-    if (id >= 0 and id <=50):
-        dictionary = cv.aruco.Dictionary_get(cv.aruco.DICT_6X6_250)
-        cv.aruco.drawMarker(dictionary, id, 200, markerImage, 1)
-        cv.imwrite("marker23.jpg", markerImage)
-        return True
-    else:
-        return False
 
 
 def extract_panel_bounds(image):
     """
-    Extracts the top-left, bottom-right coordinates of a rectangular raster at the center 
-    of the reflective panel 
-    :param image: The numpy array of the image
-    :return: list of an x,y tuple of top-left and bottom-right pixels
+    Detects an Aruco marker attached to a reflectance calibration panel and calculates the location of the panel itself.
+
+    It is important to note that the function relies on an orientation of the calibration panel in which:
+    (1) The orientation of the Aruco marker is rotated 90 degrees clock-wise from the standard orientation,
+    with its top left corner forming the top right of the marker in the image.
+    (2) The reflectance panel itself is situated directly above the Aruco marker in the image.
+
+    :param image: The NumPy array of the image
+    :return: The non-rotated bounding box of the panel, as a BoundingBox object
     """
     # o------>  +X
     # |
     # |
     # v +Y
     dictionary = cv.aruco.Dictionary_get(cv.aruco.DICT_6X6_250)
-    
-    corners, ids, rejectedImgPoints	= cv.aruco.detectMarkers(image, dictionary)
+    corners, ids, rejected_img_points = cv.aruco.detectMarkers(image, dictionary)
+
     # if at least one marker detected
-    if (not ids is None):
+    if ids is not None:
         aruco_side_length_p = cv.norm(corners[0][0][1] - corners[0][0][0])
         gsd = ARUCO_SIDE_LENGTH_M / aruco_side_length_p
-        print(f"GSD: {gsd:10.5f} m/pixel")
+        logger.debug(f"Calibration image GSD: {gsd:10.5f} m/pixel")
 
         top_aruco_line = corners[0][0][0] - corners[0][0][3]
         top_aruco_line_middle = top_aruco_line / 2.0 + corners[0][0][3]
@@ -77,16 +77,11 @@ def extract_panel_bounds(image):
         sample_center = top_aruco_line_middle + top_aruco_line_normal_scaled_pixels
         sample_top_left_corner = np.array([sample_center[0] - (SAMPLE_RECT_HEIGHT / 2.0) / gsd, sample_center[1] - (SAMPLE_RECT_WIDTH / 2.0) / gsd])
 
-        top_left_x = int(sample_top_left_corner[0])
-        top_left_y = int(sample_top_left_corner[1])
-        bottom_right_x = top_left_x + int(sample_top_left_corner[0] + SAMPLE_RECT_WIDTH / gsd)
-        bottom_right_y = top_left_y + int(sample_top_left_corner[1] + SAMPLE_RECT_HEIGHT / gsd)
-
-        rectangle_position = [(top_left_x, top_left_y),
-                              (bottom_right_x, bottom_right_y)]
-        
-        return rectangle_position      
-    return None
+        top_left = (int(sample_top_left_corner[0]), int(sample_top_left_corner[1]))
+        bottom_right = (int(top_left[0] + SAMPLE_RECT_WIDTH / gsd), int(top_left[1] + SAMPLE_RECT_HEIGHT / gsd))
+        return BoundingBox(top_left=top_left, bottom_right=bottom_right)
+    else:
+        return None
 
 
 def get_reflectance(image_path):
@@ -100,13 +95,12 @@ def get_reflectance(image_path):
     # OpenCV aruco detection only accepts 8-bit data
     image_8bit = convert_to_type(image_16bit, np.uint8)
 
-    rectangle_position = extract_panel_bounds(image_8bit)
-    if rectangle_position is None:
+    panel = extract_panel_bounds(image_8bit)
+    if panel is None:
         logger.info("No reflectance panel found. Mean DN: NaN")
         return np.nan
     else:
-        reflectance_pixels = image_16bit[rectangle_position[0][0]:rectangle_position[1][0],
-                                         rectangle_position[0][1]:rectangle_position[1][1]]
+        reflectance_pixels = image_16bit[panel.bounds()]
         mean_reflectance_digital_number = reflectance_pixels.mean()
 
         logger.info(f"Mean DN: {mean_reflectance_digital_number:10.5}")
