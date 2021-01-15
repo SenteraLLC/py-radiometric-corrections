9499becbbc02965fac6e1541e9ddef37464949ea
unknown <cliff.dellamano@sentera.com>
2021-01-15 14:54:00 -0600

    Fix bugs

--- correct6x/cli.py
+++ correct6x/cli.py
@@ -9,62 +9,14 @@ import imgparse
 import correct6x
 
 
-@click.group()
-@click.option(
-    "--log_level",
-    type=click.Choice(["DEBUG", "INFO", "WARNING"]),
-    default="INFO",
-    help="Set logging level for both console and file",
-)
-@click.pass_context
-def cli(ctx, log_level):
-    """CLI entrypoint."""
-    logging.basicConfig(level=log_level)
+def _correct_6x_images(input_path,
+                       calibration_id,
+                       output_path,
+                       no_ils_correct,
+                       no_reflectance_correct,
+                       delete_original,
+                       exiftool_path):
     logger = logging.getLogger()
-
-    ctx.ensure_object(dict)
-    ctx.obj['logger'] = logger
-
-
-@cli.command()
-@click.argument("input_path",
-                help='Path to single-band, single-page .TIF files taken from a Sentera 6X sensor. Providing a '
-                     'file path to the original multi-page images is not currently supported. However, '
-                     'specifying a folder containing all single-page files in their respective sub-folders '
-                     'will cause the script to perform ILS correction recursively throughout each sub-folder.')
-@click.option("--calibration_id", default='CAL',
-              help='Identifier in the name of the image that denotes it is from the calibration set. '
-                   'If not specified, defaults to "CAL".')
-@click.option("--output_path",
-              help='Path to output folder at which the corrected images will be stored. If not supplied, '
-                   'corrected images will be placed into the input directory.')
-@click.option("--no_ils_correct",
-              default=False,
-              is_flag=True,
-              help="If selected, ILS correction will not be applied to the images.")
-@click.option("--no_reflectance_correct",
-              default=False,
-              is_flag=True,
-              help="If selected, reflectance correction will not be applied to the images.")
-@click.option("--delete_original",
-              default=False,
-              is_flag=True,
-              help='Overwrite original 12-bit images with the corrected versions. If selected, corrected '
-                   'images are renamed to their original names. If not, an extension is added.')
-@click.option("--exiftool_path",
-              help="Path to ExifTool executable. ExifTool is required for the conversion; if not passed, "
-                   "the script will use a bundled ExifTool executable.")
-@click.pass_context
-def run(ctx,
-        input_path,
-        calibration_id,
-        output_path,
-        no_ils_correct,
-        no_reflectance_correct,
-        delete_original,
-        exiftool_path):
-
-    logger = ctx.obj['logger']
 
     def _flag_format(flag):
         if flag:
@@ -79,7 +31,7 @@ def run(ctx,
             # path into variable _MEIPASS'.
             exiftool_path = os.path.join(sys._MEIPASS, 'exiftool.exe')
         else:
-            exiftool_path = os.path.join(os.path.dirname(os.path.dirname(os.path.abspath(__file__))),
+            exiftool_path = os.path.join(os.path.dirname(os.path.abspath(__file__)),
                                          'exiftool',
                                          'exiftool.exe')
         logger.info("Using bundled executable. Setting ExifTool path to %s", exiftool_path)
@@ -128,7 +80,59 @@ def run(ctx,
         correct6x.move_corrected_images(image_df)
 
 
+@click.group()
+@click.option(
+    "--log_level",
+    type=click.Choice(["DEBUG", "INFO", "WARNING"]),
+    default="INFO",
+    help="Set logging level for both console and file",
+)
+def cli(log_level):
+    """CLI entrypoint."""
+    logging.basicConfig(level=log_level)
+
+
+@cli.command()
+@click.argument("input_path")
+@click.option("--calibration_id", default='CAL',
+              help='Identifier in the name of the image that denotes it is from the calibration set. '
+                   'If not specified, defaults to "CAL".')
+@click.option("--output_path",
+              help='Path to output folder at which the corrected images will be stored. If not supplied, '
+                   'corrected images will be placed into the input directory.')
+@click.option("--no_ils_correct",
+              default=False,
+              is_flag=True,
+              help="If selected, ILS correction will not be applied to the images.")
+@click.option("--no_reflectance_correct",
+              default=False,
+              is_flag=True,
+              help="If selected, reflectance correction will not be applied to the images.")
+@click.option("--delete_original",
+              default=False,
+              is_flag=True,
+              help='Overwrite original 12-bit images with the corrected versions. If selected, corrected '
+                   'images are renamed to their original names. If not, an extension is added.')
+@click.option("--exiftool_path",
+              help="Path to ExifTool executable. ExifTool is required for the conversion; if not passed, "
+                   "the script will use a bundled ExifTool executable.")
+def run(**kwargs):
+    """
+    Run image corrections on a folder of 6X imagery.
+
+    Expects a path to single-band, single-page .TIF files taken from a Sentera 6X sensor. Providing a
+    file path to the original multi-page images is not currently supported. However,
+    specifying a folder containing all single-page files in their respective sub-folders
+    will cause the script to perform ILS correction recursively throughout each sub-folder.
+    """
+    _correct_6x_images(**kwargs)
+
+
 @cli.command()
 def version():
     """Print application version."""
     print(f"correct6x version\t{correct6x.__version__}")
+
+
+if __name__ == "__main__":
+    cli(sys.argv[1:])
