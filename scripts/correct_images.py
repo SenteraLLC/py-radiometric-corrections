8293d26b215bb5e763b9b8d8524941db8c7793e4
Samuel Williams <samuelcwill@gmail.com>
2022-01-28 09:41:17 -0600

    Remove imgreg as a dependency of this library.

--- scripts/correct_images.py
+++ scripts/correct_images.py
@@ -5,8 +5,6 @@ import os
 import sys
 from tqdm import tqdm
 
-from glob import glob
-
 import imgparse
 import imgcorrect
 
@@ -92,17 +90,6 @@ def correct_images(input_path, calibration_id, output_path, no_ils_correct, no_r
         if (output_path and (output_path != input_path)) or delete_original:
             imgcorrect.move_corrected_images(image_df)
 
-        # Perform registration:
-        if register:
-            from imgreg import multi_spect_dataset_handling
-            output_path = output_path or input_path
-            dataset_handler = multi_spect_dataset_handling.data_set_handler(
-                "cfg/reg_config.ini",
-                input_dataset_path=output_path,
-                output_dataset_path=os.path.join(output_path, "registered"),
-                failure_dataset_path=os.path.join(output_path, "failure")
-            )
-            dataset_handler.process_all_images(use_init_transform=True, update_from_previous=True)
     except:
         image_df.temp_path.apply(os.remove)
         raise
@@ -133,9 +120,6 @@ if __name__ == '__main__':
     parser.add_argument('--exiftool_path', '-e', default=None,
                         help="Path to ExifTool executable. ExifTool is required for the conversion; if not passed, "
                              "the script will use a bundled ExifTool executable.")
-    parser.add_argument('--register', '-R', action='store_true',
-                        help="If selected, perform image registration. Aligned images are output to "
-                             "output_path/registered or output_path/failure depending on registration success.")
 
     args = parser.parse_args()
 
