f2302d35de4ac0658db73a9696bfb261584f8168
Joey Franck <joseph.franck@sentera.com>
2025-09-19 16:41:11 -0500

    finalize gui

--- scripts/correct_images_gui.py
+++ scripts/correct_images_gui.py
@@ -11,13 +11,12 @@ class CorrectImagesApp(tk.Tk):
         super().__init__()
         self.title("Sentera Radiometric Corrections")
         self.geometry("650x500")
+        self.iconbitmap("corrections_gui_icon.ico")
         self.create_widgets()
 
     def create_widgets(self):
         row = 0
-        tk.Label(self, text="Input Path (folder)").grid(
-            row=row, column=0, sticky="w", padx=5
-        )
+        tk.Label(self, text="Input Path").grid(row=row, column=0, sticky="w", padx=15)
         self.input_path_var = tk.StringVar()
         tk.Entry(
             self, textvariable=self.input_path_var, width=50, justify="right"
@@ -27,9 +26,7 @@ class CorrectImagesApp(tk.Tk):
         )
         row += 1
 
-        tk.Label(self, text="Output Path (folder)").grid(
-            row=row, column=0, sticky="w", padx=5
-        )
+        tk.Label(self, text="Output Path").grid(row=row, column=0, sticky="w", padx=15)
         self.output_path_var = tk.StringVar()
         tk.Entry(
             self, textvariable=self.output_path_var, width=50, justify="right"
@@ -39,64 +36,73 @@ class CorrectImagesApp(tk.Tk):
         )
         row += 1
 
-        tk.Label(self, text="Calibration ID").grid(
-            row=row, column=0, sticky="w", padx=5
-        )
-        self.calibration_id_var = tk.StringVar(value="CAL")
-        tk.Entry(self, textvariable=self.calibration_id_var).grid(
-            row=row, column=1, padx=(0, 5)
-        )
-        row += 1
-
-        self.no_ils_var = tk.BooleanVar(value=True)
-        tk.Checkbutton(self, text="No ILS Correction", variable=self.no_ils_var).grid(
-            row=row, column=0, sticky="w", padx=5
-        )
-        row += 1
-
-        self.no_reflectance_var = tk.BooleanVar()
+        self.reflectance_var = tk.BooleanVar(value=True)
         tk.Checkbutton(
-            self, text="No Reflectance Correction", variable=self.no_reflectance_var
-        ).grid(row=row, column=0, sticky="w", padx=5)
+            self, text="Reflectance Correction", variable=self.reflectance_var
+        ).grid(row=row, column=0, sticky="w", padx=15)
+
+        self.ils_var = tk.BooleanVar(value=False)
+        tk.Checkbutton(self, text="ILS Correction", variable=self.ils_var).grid(
+            row=row, column=1, sticky="w", padx=15
+        )
+        row += 1
+
+        self.advanced_options = tk.BooleanVar()
+        tk.Checkbutton(
+            self,
+            text="Advanced Options",
+            variable=self.advanced_options,
+            command=self.toggle_advanced_options,
+        ).grid(row=row, column=0, sticky="w", padx=15)
+
+        row += 1
+
+        self.exiftool_path_var = tk.StringVar()
+        self.exiftool_path_label = tk.Label(self, text="ExifTool Path (optional)")
+        self.exiftool_path_label.grid(row=row, column=0, sticky="w", padx=15)
+        self.exiftool_entry = tk.Entry(
+            self, textvariable=self.exiftool_path_var, width=50, justify="right"
+        )
+        self.exiftool_entry.grid(row=row, column=1, sticky="w", padx=(0, 5))
+        self.exiftool_path_browse_button = tk.Button(
+            self, text="Browse", command=self.browse_exiftool
+        )
+        self.exiftool_path_browse_button.grid(row=row, column=2)
+        row += 1
+
+        self.calibration_id_var = tk.StringVar(value="CAL")
+        self.cal_id_label = tk.Label(self, text="Calibration ID")
+        self.cal_id_label.grid(row=row, column=0, sticky="w", padx=15)
+        self.cal_id_entry = tk.Entry(self, textvariable=self.calibration_id_var)
+        self.cal_id_entry.grid(row=row, column=1, sticky="w")
         row += 1
 
         self.all_panels_var = tk.BooleanVar()
-        tk.Checkbutton(
+        self.all_panels_checkbutton = tk.Checkbutton(
             self,
-            text="Use All Panels for Reflectance Correction",
+            text="Use All Panels sets(6X)",
             variable=self.all_panels_var,
-        ).grid(row=row, column=0, sticky="w", padx=5)
+        )
+        self.all_panels_checkbutton.grid(row=row, column=0, sticky="w", padx=15)
         row += 1
 
         self.delete_original_var = tk.BooleanVar()
-        tk.Checkbutton(
+        self.delete_overwrite_checkbutton = tk.Checkbutton(
             self,
             text="Delete/Overwrite Original Images",
             variable=self.delete_original_var,
-        ).grid(row=row, column=0, sticky="w", padx=5)
-        row += 1
-
-        tk.Label(self, text="ExifTool Path (optional)").grid(
-            row=row, column=0, sticky="w", padx=5
         )
-        self.exiftool_path_var = tk.StringVar()
-        tk.Entry(self, textvariable=self.exiftool_path_var, width=40).grid(
-            row=row, column=1, padx=(0, 5)
-        )
-        tk.Button(self, text="Browse", command=self.browse_exiftool).grid(
-            row=row, column=2
-        )
+        self.delete_overwrite_checkbutton.grid(row=row, column=0, sticky="w", padx=15)
         row += 1
 
         self.uint16_var = tk.BooleanVar()
-        tk.Checkbutton(
+        self.uint16_checkbutton = tk.Checkbutton(
             self, text="Output as uint16 (0-65535)", variable=self.uint16_var
-        ).grid(row=row, column=0, sticky="w", padx=5)
-        row += 1
+        )
+        self.uint16_checkbutton.grid(row=row, column=0, sticky="w", padx=15)
 
-        # Output text box
-        self.output_text = tk.Text(self, height=10, width=70)
-        self.output_text.grid(row=row, column=0, columnspan=3)
+        self.toggle_advanced_options()
+
         row += 1
 
         # Run Correction button at the bottom
@@ -109,6 +115,28 @@ class CorrectImagesApp(tk.Tk):
         )
         self.run_button.grid(row=row, column=0, columnspan=3, pady=20)
         row += 1
+
+        # Output text box
+        self.output_text = tk.Text(self, height=10, width=70)
+        self.output_text.grid(row=row, column=0, sticky="ew", columnspan=3, padx=25)
+
+    def toggle_advanced_options(self):
+        widgets = [
+            self.cal_id_label,
+            self.cal_id_entry,
+            self.all_panels_checkbutton,
+            self.delete_overwrite_checkbutton,
+            self.exiftool_path_label,
+            self.exiftool_entry,
+            self.exiftool_path_browse_button,
+            self.uint16_checkbutton,
+        ]
+        if not self.advanced_options.get():
+            for widget in widgets:
+                widget.grid_remove()
+        else:
+            for widget in widgets:
+                widget.grid()
 
     def browse_input(self):
         path = filedialog.askdirectory()
@@ -152,9 +180,9 @@ class CorrectImagesApp(tk.Tk):
             cmd += ["--calibration_id", self.calibration_id_var.get()]
         if self.output_path_var.get():
             cmd += ["--output_path", self.output_path_var.get()]
-        if self.no_ils_var.get():
+        if not self.ils_var.get():
             cmd.append("--no_ils_correct")
-        if self.no_reflectance_var.get():
+        if not self.reflectance_var.get():
             cmd.append("--no_reflectance_correct")
         if self.all_panels_var.get():
             cmd.append("--all_panels")
