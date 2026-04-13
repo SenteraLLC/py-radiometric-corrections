43814854bb938c3c07e3a7d543d168950a6b1421
HuPengcheng <hpc0813@outlook.com>
2026-04-13 18:29:16 +0800

    fix the issue#70: Radiometrically corrected M3M images fail to mosaic correctly in ODM.
    Signed-off-by: HuPengcheng <hpc0813@outlook.com>

--- imgcorrect/metadata.py
+++ imgcorrect/metadata.py
@@ -18,25 +18,69 @@ def copy_exif(image_df_row, exiftool_path):
         "-TagsFromFile",
         image_df_row.image_path,
         "-all",
-        "--xmp-Camera:ColorTransform",
-        "--xmp-Camera:SunSensor",
-        "-xmp-Camera:IsNormalized=1",
-        "-xmp-Camera:BlackCurrent=",
-        "-xmp-Camera:BlackCurrent=0",
     ]
-    if image_df_row.reduce_xmp:
-        parser = MetadataParser(image_df_row.image_path)
-        cent_arr, fwhm_arr = parser.wavelength_data()
-        band_arr = parser.bandnames()
-        i = int(image_df_row.XMP_index)
+    # DJI Mavic 3M
+    if image_df_row.sensor == "M3M":
+        BAND_PARAMS = {
+            "Green": {
+                "name": "Green",
+                "wavelength": 560,
+                "fwhm": 16,
+                # "freq": "560(+/-16)nm",
+            },
+            "Red": {
+                "name": "Red",
+                "wavelength": 650,
+                "fwhm": 16,
+                # "freq": "650(+/-16)nm",
+            },
+            "RedEdge": {
+                "name": "RedEdge",
+                "wavelength": 730,
+                "fwhm": 16,
+                # "freq": "730(+/-16)nm",
+            },
+            "NIR": {
+                "name": "NIR",
+                "wavelength": 860,
+                "fwhm": 26,
+                # "freq": "860(+/-26)nm",
+            },
+        }
+        band_info = BAND_PARAMS[image_df_row.band]
+
         command += [
-            "-xmp-Camera:BandName=",
-            "-xmp-Camera:CentralWavelength=",
-            "-xmp-Camera:WavelengthFWHM=",
-            f"-xmp-Camera:BandName={band_arr[i]}",
-            f"-xmp-Camera:CentralWavelength={cent_arr[i]}",
-            f"-xmp-Camera:WavelengthFWHM={fwhm_arr[i]}",
+            "-BandName=",
+            "-CentralWavelength=",
+            "-WavelengthFWHM=",
+            f"-BandName={band_info['name']}",
+            f"-CentralWavelength={band_info['wavelength']}",
+            f"-WavelengthFWHM={band_info['fwhm']}",
+            # f"-BandFreq={band_info['freq']}",
+            f"-IsNormalized=1",
         ]
+    else:
+        command += [
+            "--xmp-Camera:ColorTransform",
+            "--xmp-Camera:SunSensor",
+            "-xmp-Camera:IsNormalized=1",
+            "-xmp-Camera:BlackCurrent=",
+            "-xmp-Camera:BlackCurrent=0",
+        ]
+
+        if image_df_row.reduce_xmp:
+            parser = MetadataParser(image_df_row.image_path)
+            cent_arr, fwhm_arr = parser.wavelength_data()
+            band_arr = parser.bandnames()
+            i = int(image_df_row.XMP_index)
+            command += [
+                "-xmp-Camera:BandName=",
+                "-xmp-Camera:CentralWavelength=",
+                "-xmp-Camera:WavelengthFWHM=",
+                f"-xmp-Camera:BandName={band_arr[i]}",
+                f"-xmp-Camera:CentralWavelength={cent_arr[i]}",
+                f"-xmp-Camera:WavelengthFWHM={fwhm_arr[i]}",
+            ]
     command.append(image_df_row.temp_path)
 
     # Use CREATE_NO_WINDOW flag on Windows if available (Python 3.7+)
