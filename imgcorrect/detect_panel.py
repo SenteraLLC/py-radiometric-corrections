bcbb0c5d616ec6b7712b6254025968aa3de8fd17
Dimitris <dimitris.zermas@sentera.com>
2026-06-01 11:55:34 -0500

    Add fourth aruco (ID 287, batch4 coefficients)

--- imgcorrect/detect_panel.py
+++ imgcorrect/detect_panel.py
@@ -61,11 +61,13 @@ def extract_panel_bounds(image):
     # |
     # |
     # v +Y
-    dictionary = cv.aruco.Dictionary_get(cv.aruco.DICT_6X6_250)
+    dictionary = cv.aruco.Dictionary_get(cv.aruco.DICT_6X6_1000)
     corners, ids, rejected_img_points = cv.aruco.detectMarkers(image, dictionary)
 
     # if at least one marker detected
-    if ids is not None and (ids[0][0] == 23 or ids[0][0] == 63 or ids[0][0] == 217):
+    if ids is not None and (
+        ids[0][0] == 23 or ids[0][0] == 63 or ids[0][0] == 217 or ids[0][0] == 287
+    ):
         aruco_side_length_p = cv.norm(corners[0][0][1] - corners[0][0][0])
         gsd = ARUCO_SIDE_LENGTH_M / aruco_side_length_p
         logger.debug("Calibration image GSD: %10.5f m/pixel", gsd)
@@ -76,9 +78,9 @@ def extract_panel_bounds(image):
 
         # panels with markers of id:63 are incorrectly oriented
         # in this case, panel is beneath marker
-        if ids[0][0] == 63 or ids[0][0] == 217:
+        if ids[0][0] == 63 or ids[0][0] == 217 or ids[0][0] == 287:
             logger.info(
-                "Aruco ID: 63 or 217 detected. Adjusting for panel beneath marker."
+                "Aruco ID: 63, 217, or 287 detected. Adjusting for panel beneath marker."
             )
             top_aruco_line = corners[0][0][3] - corners[0][0][2]
             top_aruco_line_middle = top_aruco_line / 2.0 + corners[0][0][2]
